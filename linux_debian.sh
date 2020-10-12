812e6e8ee17544994a563032ce64bbdba8d137b3
Anton Zorin <20819916+tonyzorin@users.noreply.github.com>
2020-10-12 16:11:32 +0300

    Update linux_debian.sh

--- linux_debian.sh
+++ linux_debian.sh
@@ -1,18 +1,6 @@
 #! /bin/sh
-url="AGENT_URL"
+url="$2"
 filename=$(basename "$url")
 workpath=$(dirname $(readlink -f $0))
-if (test -f /usr/bin/curl)
-then
-	curl $url -o $workpath/$filename -s
-else
-	if (test -f /usr/bin/wget)
-	then
-		wget -q $url -O $workpath/$filename
-	else
-		echo "No wget or curl installed, installing curl now"
-		sudo apt-get -y install curl
-		curl $url -o $workpath/$filename -s
-	fi
-fi
+curl $url -o $workpath/$filename -s
 sudo dpkg --install  $workpath/$filename
